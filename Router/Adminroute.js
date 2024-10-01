70c39d22160cf331906e3164208755cad9395ab8
Amit-Bongale <amitbongale14@gmail.com>
2024-10-01 14:53:06 +0530

    admin route created

---
+++ Router/Adminroute.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+
+
+router.post('/create' , async (req, res) => {
+    try {
+        const admin = await Admininfo.create({
+            'id': 'A003',
+            'name': 'apoorva',
+            'email': 'abcd@gmail.com',
+            'password': 'a@123',
+            'phone': 1234567890,
+            'address': 'xyz'
+        });
+        res.json(admin);
+        console.log('Inserted:', admin);
+    } catch (error) {
+        console.error("Insertion Error:", error); // Log full error
+        res.status(500).json({ message: 'Error inserting admin', error: error.message });
+    }
+})
+
+
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
-const Admininfo = require('./Models/Admin/Admininfo')
-
 const env = require('dotenv');
 env.config()
-
 const port = 3000 || process.env.PORT
-
 const app = express()
-
 app.use(cors());
-
 app.use(express.json());
-
 
 mongoose.connect(process.env.DBCONNECTION)
 .then(() => console.log("Connected to Database"))
@@ -23,20 +15,11 @@ mongoose.connect(process.env.DBCONNECTION)
 app.listen(port , () => {
     console.log("Server is Running on port 3000")
 })
-app.post('/createadmin', async (req, res) => {
-    try {
-        const admin = await Admininfo.collection.insertOne({
-            'id': 'A001', 
-            'name': 'Amit', 
-            'email': 'abcd@gmail.com', 
-            'password': 'a@123', 
-            'phone': 1234567890, 
-            'address': 'xyz'
-        });
-        res.json(admin);
-        console.log('Inserted:', admin);
-    } catch (error) {
-        console.error("Insertion Error:", error); // Log full error
-        res.status(500).json({ message: 'Error inserting admin', error: error.message });
-    }
-});
+
+
+
+const Adminroute = require('./Router/Adminroute')
+
+
+
+app.use('/admin' , Adminroute) 
