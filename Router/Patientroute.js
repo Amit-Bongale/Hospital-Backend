abd2854cd169a29aad543b113103828da0c67a79
Amit-Bongale <amitbongale14@gmail.com>
2024-10-31 17:33:01 +0530

    patient registration and login done

--- Router/Patientroute.js
+++ Router/Patientroute.js
@@ -1,14 +1,22 @@
 const express = require('express')
 const router = express.Router()
-const Patientinfo = require('../Models/Patientinfo')
+const Patient = require('../Models/Patientinfo')
 
+
+// create new Patient
 router.post('/createnewpatient', async (req, res)=>{
-    try {
-      const { id, name, gender, email, phone, dob, age, address, emergencycontact,  bloodgroup, aadharno, medicalhistory, } = req.body;
-      
-      console.log(req.body)
+  try {
+    const { id, name, gender, email, phone, dob, age, address, emergencycontact,  bloodgroup, aadharno, medicalhistory, } = req.body;
 
-      const patient = await Patientinfo.create({
+    const existingPatient = await Patient.findOne({ 'id' : id });
+
+    if (existingPatient) {
+      return res.status(400).json({ message: 'A Patient with this ID already exists.'});
+    }
+    
+    console.log(req.body)
+
+    const patient = await Patient.create({
       'id' : id,
       'name' : name,
       'gender' : gender,
@@ -32,16 +40,54 @@ router.post('/createnewpatient', async (req, res)=>{
 })
 
 
+// create user
+router.post('/createnewuser', async (req, res)=>{
+  try {
+    const { id, name, gender, email, phone, dob, age, address, emergencycontact,  bloodgroup, aadharno, medicalhistory, password } = req.body;
+    
+    console.log(req.body)
+
+    const existingPatient = await Patient.findOne({ 'id' : id });
+
+    if (existingPatient) {
+      return res.status(400).json({ message: 'An account with this Details already exists. Please login' });
+    }
+
+    const patient = await Patient.create({
+      'id' : id,
+      'name' : name,
+      'gender' : gender,
+      'phone' : phone,
+      'email' : email,
+      'dob' : dob,
+      'age' : age,
+      'emergencycontact' : emergencycontact,
+      'address' : address,
+      'bloodgroup' : bloodgroup,
+      'aadharno' : aadharno,
+      'medicalhistory' : medicalhistory,
+      'password' : password,
+    });
+    res.status(200).json({success: true, message: 'Account Created successfully, Please Login to Continue', patient });
+    console.log('Inserted:', patient);
+  }
+  catch (error) {
+    console.error("Insertion Error:", error);
+    res.status(500).json({ message: 'Error Creating Account, please Try Again', error: error.message });  
+  }
+})
+
+
 // get details all patients
 router.post('/allpatients', async (req, res) => {
-    try {
-      const patient = await Patientinfo.find(); // Fetch all patient from the collection
-      res.status(200).json(patient);
-      // console.log(patient)
-    } catch (error) {
-      console.error("Error fetching patient:", error);
-      res.status(500).json({ message: 'Error fetching patient', error: error.message });
-    }
+  try {
+    const patient = await Patient.find(); // Fetch all patient from the collection
+    res.status(200).json(patient);
+    // console.log(patient)
+  } catch (error) {
+    console.error("Error fetching patient:", error);
+    res.status(500).json({ message: 'Error fetching patient', error: error.message });
+  }
 });
 
 
@@ -52,10 +98,9 @@ router.post('/findpatient/:id', async (req, res) => {
     console.log(id)
     
     try {
-      // Find one patient by the provided id
-      const patient = await Patientinfo.findOne({ 'id' : id });
+      // Find one patient by the  id
+      const patient = await Patient.findOne({ 'id' : id });
   
-      // If no patient is found, return a 404 error
       if (!patient) {
         return res.status(404).json({ message: `patient not found ${id}` });
       }
@@ -72,53 +117,84 @@ router.post('/findpatient/:id', async (req, res) => {
 
 // Update detaiils of patient with id
 router.post('/updatepatient/:id', async (req, res) => {
-    const { id } = req.params;
-    const updateData = req.body;
+  const { id } = req.params;
+  const updateData = req.body;
+  
+  try {
+    // Find the patient by id and update with new data
+    const updatedpatient = await Patient.findOneAndUpdate(
+      { id: id },  // Find patient by id
+      { $set: updateData },  // Update the patient's details with the new data
+      { new: true } // Return the updated document
+    );
     
-    try {
-      // Find the patient by id and update with new data
-      const updatedpatient = await Patientinfo.findOneAndUpdate(
-        { id: id },  // Find patient by id
-        { $set: updateData },  // Update the patient's details with the new data
-        { new: true } // Return the updated document
-      );
-      
-      // If no patient found, return a 404 error
-      if (!updatedpatient) {
-        return res.status(404).json({ message: 'patient not found' });
-      }
-  
-      // Return the updated patient
-      res.status(200).json({message: 'Details Updated Sucessfully' , updatedpatient});
-      console.log(updatedpatient)
-  
-    } catch (error) {
-      console.error('Error updating patient:', error);
-      res.status(500).json({ message: 'Error updating patient', error: error.message });
+    // If no patient found, return a 404 error
+    if (!updatedpatient) {
+      return res.status(404).json({ message: 'patient not found' });
     }
+
+    // Return the updated patient
+    res.status(200).json({message: 'Details Updated Sucessfully' , updatedpatient});
+    console.log(updatedpatient)
+
+  } catch (error) {
+    console.error('Error updating patient:', error);
+    res.status(500).json({ message: 'Error updating patient', error: error.message });
+  }
 });
 
 
 
 // DELETE route to delete a patient by Id
 router.post('/deletepatient', async (req, res) => {
-    try {
-      const {id} = req.body; 
-      console.log(id)
+  try {
+    const {id} = req.body; 
+    console.log(id)
 
-      // Deleting a patient by Id
-      const patient = await Patientinfo.findOneAndDelete({ 'id' : id});
-  
-      if (!patient) {
-        return res.status(404).json({ message: "patient not found" });
-      }
-  
-      res.status(200).json({ message: "patient deleted successfully" });
+    // Deleting a patient by Id
+    const patient = await Patient.findOneAndDelete({ 'id' : id});
 
-    } catch (error) {
-      res.status(500).json({ message: "Error deleting patient", error: error.message });
-      console.log(error.message);
+    if (!patient) {
+      return res.status(404).json({ message: "patient not found" });
     }
+
+    res.status(200).json({ message: "patient deleted successfully" });
+
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting patient", error: error.message });
+    console.log(error.message);
+  }
+});
+
+
+router.post('/login', async (req, res) => {
+
+  const { id, password } = req.body;
+
+  try {
+    // Check if the user exists
+    const user = await Patient.findOne({'id' : id });
+    if (!user) {
+      return res.status(404).json({ success: false, message: 'Account Does not Exist' });
+    }
+
+    // Check if the password matches
+    if (user.password !== password) {
+      return res.status(401).json({ success: false, message: 'Invalid password' });
+    }
+
+    // Respond with success message
+    res.status(200).json({ success: true, message: 'Login successful',
+      user: { 
+        id: user.id, 
+        name: user.name,
+        // You can include other user details as needed
+      }
+    });
+  } catch (error) {
+    console.error("Login error:", error);
+    res.status(500).json({ success: false, message: 'Server error' });
+  }
 });
 
 
