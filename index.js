7b2d7fb55e8e452f59b25e2e2f5c81692a8e366c
Amit-Bongale <amitbongale14@gmail.com>
2025-02-09 15:49:39 +0530

    cors acess updated

--- index.js
+++ index.js
@@ -1,55 +1,61 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-const env = require('dotenv');
-env.config()
+const env = require("dotenv");
+env.config();
 
-const port = process.env.PORT || 3000
-const app = express()
+const port = process.env.PORT || 3000;
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 // enable cors for deployed site
-app.use(cors({ origin: "https://hospital-management-system-x1n5.onrender.com/" }));
+app.use(
+  cors({
+    origin: "https://hospital-management-system-x1n5.onrender.com",
+    credentials: true, // Allow cookies/auth header
+  })
+);
 
 mongoose.connect(process.env.DBCONNECTION)
 .then(() => console.log("Connected to Database"))
-.catch((e) => console.log("Error connecting to db" , e))
+.catch((e) => console.log("Error connecting to db", e));
 
-app.listen(port , () => {
-    console.log("Server is Running on port 3000")
-})
-
-
-const Adminroute = require('./Router/Adminroute.js')
-const Doctorroute = require('./Router/Doctorroute.js')
-const Patientroute = require('./Router/Patientroute.js')
-const Staffroute = require('./Router/Staffroute.js')
-const Appointment = require('./Router/AppointmentRoute.js')
-const salaryroute = require('./Router/Salaryroute.js')
-const Testroute = require('./Router/Testroute.js')
-const Queueroute = require('./Router/Queueroute.js')
-const Contactusroute = require('./Router/ContactusRoute.js')
-const Admissionroute = require('./Router/AdmissionRoute.js')
-const Medicalhistoryroute = require('./Router/MedicalhistoryRoute.js')
-const Billroute = require('./Router/BillRoute.js')
-const Wardroute = require('./Router/WardRoute.js') 
+app.listen(port, () => {
+  console.log("Server is Running on port 3000");
+});
 
 
- 
+// Routes
 
-app.use('/admin' , Adminroute) 
-app.use('/doctor' , Doctorroute)
-app.use('/patient' , Patientroute)
-app.use('/staff' , Staffroute)
-app.use('/appointment' , Appointment)
-app.use('/salary' , salaryroute)
-app.use('/test', Testroute)
-app.use('/queue', Queueroute)
-app.use('/contactus' , Contactusroute)
-app.use('/admission' , Admissionroute)
-app.use('/medicalhistory' , Medicalhistoryroute)
-app.use('/bill' , Billroute)
-app.use('/ward' , Wardroute)
+const Adminroute = require("./Router/Adminroute.js");
+const Doctorroute = require("./Router/Doctorroute.js");
+const Patientroute = require("./Router/Patientroute.js");
+const Staffroute = require("./Router/Staffroute.js");
+const Appointment = require("./Router/AppointmentRoute.js");
+const salaryroute = require("./Router/Salaryroute.js");
+const Testroute = require("./Router/Testroute.js");
+const Queueroute = require("./Router/Queueroute.js");
+const Contactusroute = require("./Router/ContactusRoute.js");
+const Admissionroute = require("./Router/AdmissionRoute.js");
+const Medicalhistoryroute = require("./Router/MedicalhistoryRoute.js");
+const Billroute = require("./Router/BillRoute.js");
+const Wardroute = require("./Router/WardRoute.js");
+
+
+
+app.use("/admin", Adminroute);
+app.use("/doctor", Doctorroute);
+app.use("/patient", Patientroute);
+app.use("/staff", Staffroute);
+app.use("/appointment", Appointment);
+app.use("/salary", salaryroute);
+app.use("/test", Testroute);
+app.use("/queue", Queueroute);
+app.use("/contactus", Contactusroute);
+app.use("/admission", Admissionroute);
+app.use("/medicalhistory", Medicalhistoryroute);
+app.use("/bill", Billroute);
+app.use("/ward", Wardroute);
